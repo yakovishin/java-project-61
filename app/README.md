8765105decc60904b5569b93e19f67485514391d
Maksim_Iakovishin <yakovishin.maks@mail.ru>
2023-08-18 16:55:29 +0300

    add checkstyle plugin in build.gradle, add README.md, add checkstyle.xml config, fix checkstyle issues

---
+++ app/README.md
@@ -0,0 +1,1 @@
+<a href="https://codeclimate.com/github/yakovishin/java-project-61/maintainability"><img src="https://api.codeclimate.com/v1/badges/c7753809b682b93113b9/maintainability" /></a> 
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 public class Cli {
     public static void sayHello() {
-        var scanner  = new Scanner(System.in);
+        var scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         var name = scanner.next();
         System.out.printf("Hello, %s!\n", name);
         scanner.close();
     }
 }
+
